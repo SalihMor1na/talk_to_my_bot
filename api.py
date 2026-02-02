27e07a7b2afc2702543367d595f74f36d6faad38
SalihMor1na <salihmorina555@gmail.com>
2026-02-02 16:06:27 +0100

    Created deletefile endpoint

--- api.py
+++ api.py
@@ -11,3 +11,7 @@ async def create_upload_file(file: UploadFile):
 
     with open(file_path, "wb") as f:
         shutil.copyfileobj(file.file, f)
+
+@app.delete("/deletefile/{filename}")
+async def delete_file(filename: str):
+    file_path = DATA_PATH / filename
