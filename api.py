fcd3faafe77d550481bf5e2e0d5b6d93b5d410c6
SalihMor1na <salihmorina555@gmail.com>
2026-02-02 16:03:42 +0100

    save uploaded files to DATA_PATH

--- api.py
+++ api.py
@@ -1,4 +1,5 @@
-from fastapi import UploadFile
+from backend.constants import DATA_PATH
+import shutil
 from fastapi import FastAPI, UploadFile
 
 app = FastAPI()
@@ -6,4 +7,7 @@ app = FastAPI()
 
 @app.post("/uploadfile/")
 async def create_upload_file(file: UploadFile):
-    return file
+    file_path = DATA_PATH / file.filename
+
+    with open(file_path, "wb") as f:
+        shutil.copyfileobj(file.file, f)
