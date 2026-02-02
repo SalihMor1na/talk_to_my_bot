3617065198bb8b14e44409e22f03f14a4c57dcf0
SalihMor1na <salihmorina555@gmail.com>
2026-02-02 16:02:06 +0100

    Created uploadfile endpoint

--- api.py
+++ api.py
@@ -1,1 +1,9 @@
 from fastapi import UploadFile
+from fastapi import FastAPI, UploadFile
+
+app = FastAPI()
+
+
+@app.post("/uploadfile/")
+async def create_upload_file(file: UploadFile):
+    return file
